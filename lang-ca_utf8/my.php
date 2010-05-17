437bf6e9fabbec4251b66b42ba75a8ef8e5d2f3e
Albert Gasset <albert.gasset@gmail.com>
2010-05-17 15:46:52 +0200

    ca_utf8: el meu campus

--- lang-ca_utf8/my.php
+++ lang-ca_utf8/my.php
@@ -2,10 +2,10 @@
       // my.php - created with Moodle 1.6.1+ (2006050511)
 
 
-$string['mymoodle'] = 'El meu Moodle';
+$string['mymoodle'] = 'El meu campus';
 $string['nocourses'] = 'No hi ha informació de cursos per mostrar.';
-$string['noguest'] = 'La pàgina \"El meu Moodle\" no està disponible per a usuaris visitants';
-$string['pinblocks'] = 'Configuració de blocs fixos per a la pàgina \"El meu Moodle\"';
-$string['pinblocksexplan'] = 'Els blocs que configureu aquí seran visibles (i no editables) per a qualsevol usuari de Moodle en la seva pàgina resum \"El meu Moodle\".';
+$string['noguest'] = 'La pàgina \"El meu campus\" no està disponible per a usuaris visitants';
+$string['pinblocks'] = 'Configuració de blocs fixos per a la pàgina \"El meu campus\"';
+$string['pinblocksexplan'] = 'Els blocs que configureu aquí seran visibles (i no editables) per a qualsevol usuari de Moodle en la seva pàgina resum \"El meu campus\".';
 
 ?>

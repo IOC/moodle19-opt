f77a5682e1f8865112f899bdd107ba65c017e672
Albert Gasset <albert.gasset@gmail.com>
2010-11-09 12:11:12 +0100

    Italian language pack (2010-05-26)

---
+++ lang-it_utf8/mediaplugin.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // mediaplugin.php - created with Moodle 1.2 Beta + (2004030702)
+
+
+$string['filtername'] = 'Plugin multimediali';
+$string['flashanimation'] = 'Animazione Flash';
+$string['flashvideo'] = 'Video Flash';
+$string['mp3audio'] = 'Audio MP3';
+
+?>

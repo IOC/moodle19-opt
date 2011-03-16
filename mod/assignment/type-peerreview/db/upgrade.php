ef6efc9c5232f7db7a4a3abe131d399da761cef8
Albert Gasset <albert.gasset@gmail.com>
2011-03-16 11:10:10 +0100

    Peer Review (2011-02-14)

---
+++ mod/assignment/type-peerreview/db/upgrade.php
@@ -0,0 +1,43 @@
+<?php  
+
+function xmldb_assignment_type_peerreview_upgrade($oldversion=0) {
+    global $CFG, $THEME, $db;
+    $result = true;
+
+    // Change type of criteria values to integers
+    if ($result && $oldversion < 2010050700) {
+        $table = new XMLDBTable('assignment_criteria');
+        $field = new XMLDBField('value');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'textshownatreview');
+        $result = $result && change_field_type($table, $field);        
+    }
+
+    // Add fields for review metrics
+    if ($result && $oldversion < 2010050500) {
+        $table = new XMLDBTable('assignment_review');
+        $field = new XMLDBField('timedownloaded');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'downloaded');
+        $result = $result && add_field($table, $field);
+        $field = new XMLDBField('timecompleted');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'complete');
+        $result = $result && add_field($table, $field);
+    }
+
+    // Add fields for measuring feedback attention
+    if ($result && $oldversion < 2011021400) {
+        $table = new XMLDBTable('assignment_review');
+        $field = new XMLDBField('timefirstviewedbyreviewee');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'reviewcomment');
+        $result = $result && add_field($table, $field);
+        $field = new XMLDBField('timelastviewedbyreviewee');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'timefirstviewedbyreviewee');
+        $result = $result && add_field($table, $field);
+        $field = new XMLDBField('timesviewedbyreviewee');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0', 'timelastviewedbyreviewee');
+        $result = $result && add_field($table, $field);
+    }
+
+    return $result;
+}
+
+?>
---
+++ mod/assignment/type-peerreview/styles.php
@@ -0,0 +1,148 @@
+<?php
+header("Content-type: text/css");
+?>
+// Style information for peerreview assignment type
+
+.progressBar {
+    margin-bottom: 20px;
+}
+
+.progressBox {
+	float: left;
+	width: 200px;
+	height: 40px;
+	margin: 10px 5px;
+}
+
+.progressNumber {
+	font-weight: bold;
+	font-size: 250%;
+	padding: 0 5px;
+	width: 20px;
+	text-align: center;
+    vertical-align: top;
+}
+
+.progressIcon {
+	width: 30px;
+	text-align: center;
+}
+
+.progressTitle {
+	font-weight: bold;
+	font-size: 120%;
+}
+
+.progressMessage {
+	color: #000000;
+    font-size: 80%;
+}
+
+.greyProgressBox .progressMessage {
+	color: #999999;
+}
+
+.redStatusBox {
+	width: 100%;
+	border-collapse: collapse;
+	border: 3px solid #ff3333;
+	background: #ffe9e9 url("images/cross.gif") no-repeat 95% 20%;
+	color: #ff3333;
+}
+
+.blueProgressBox {
+	width: 100%;
+	border-collapse: collapse;
+	border: 3px solid #3333cc;
+	background: #e9e9ff url("images/alert.gif") no-repeat 95% 20%;
+	color: #3333cc;
+}
+
+.greyProgressBox {
+	width: 100%;
+	border-collapse: collapse;
+	border: 3px solid #999999;
+	background: #e9e9e9;
+	color: #999999;
+}
+
+.greenProgressBox {
+	width: 100%;
+	border-collapse: collapse;
+	border: 3px solid #339933;
+	background: #e9ffe9 url("images/tick.gif") no-repeat 95% 20%;
+	color: #339933;
+}
+
+.errorStatus {
+	color:#ff6600;
+	background:#ffff00;
+	padding:2px;
+}
+
+.goodStatus {
+	color:#009900;
+	background:#ccffcc;
+	padding:2px;
+}
+
+.evenCriteriaRow {
+    background:#f6f6f6;
+}
+
+.criteriaCheckboxColumn {
+    vertical-align:top;
+    width:20px;
+}
+
+.criteriaTextColumn {
+    vertical-align:top;
+}
+
+.criteriaDisplayRow {
+    border-top:1px dotted #e9e9e9;
+}
+
+.criteriaDisplayColumn {
+    padding:0 0 10px 5px;
+    text-align: left;
+    vertical-align:top;
+}
+
+.reviewCommentRow {
+    text-align:left;
+    vertical-align:bottom;
+    padding:5px;
+}
+
+.reviewDetailsRow {
+    font-size:x-small;
+}
+
+.reviewDetailsRow img {
+    vertical-align:middle;
+    border:none;
+}
+
+.reviewDateColumn {
+    text-align:right;
+}
+
+.commentTextBox {
+    font-family: monospace;
+    padding:3px;
+    margin:0;
+    border:none;
+    width:99%;
+    white-space: pre-wrap;
+}
+
+.reviewStatus {
+    padding:5px 0;
+}
+
+#mod-assignment-type-peerreview-analysis .generalbox, #mod-assignment-type-peerreview-massMark .generalbox  {
+	text-align: left;
+	margin-left: auto;
+	margin-right: auto;
+}

64877a00b39c57aec1570dea907868e8cd174106
Albert Gasset <albert.gasset@gmail.com>
2011-06-28 10:15:35 +0200

    ca_utf8: atenea analytics

---
+++ lang-ca_utf8/indicadors.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * Created on 11/04/2006
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+    $string['all'] = 'Tot';
+	$string['blockname'] = 'Indicadors de Rendiment';
+	$string['modulename'] = 'Indicadors';
+	$string['modulenameplural'] = 'Indicadors';
+	$string['April'] = 'Abril';
+	$string['August'] = 'Agost';
+	$string['February'] = 'Febrer';
+	$string['December'] = 'Desembre';
+	$string['January'] = 'Gener';
+	$string['July'] = 'Juliol';
+	$string['June'] = 'Juny';
+	$string['March'] = 'Març';
+	$string['May'] = 'Maig';
+	$string['November'] = 'Novembre';
+	$string['October'] = 'Octubre';
+	$string['September'] = 'Setembre';
+	$string['day'] = 'Dia';
+	$string['hour'] = 'Hora';
+	$string['month'] = 'Mes';
+	$string['year'] = 'Any';
+	$string['from'] = 'Des de';
+	$string['to'] = 'Fins';
+	$string['gran_day'] = 'Consulta les activitats dels usuaris <BR/> amb granularitat de Dia:';
+	$string['gran_month'] = 'Consulta les activitats dels usuaris <BR/> amb granularitat de Mes:';
+    $string['gran_year'] = 'Consulta les activitats dels usuaris <BR/> amb granularitat d\'any:';
+    $string['gran_all_to']= 'Data Final : '; 
+    $string['gran_all_with']='La granularitat: ';
+    $string['gran_all'] = 'Consulta les activitats dels usuaris ';
+    $string['gran_all_from'] = 'Data Inicial : ';
+    $string['error']= 'AQUESTA OPCIO NO ESTA DISPONIBLE';
+    $string['num_user']='Nombre d\'usuaris';
+    $string['num_user_max']='Nombre màxim d\'usuaris';
+    $string['num_user_att']='Nombre d\'usuaris atesos';
+    $string['date']='Data';
+    $string['Monday'] = 'Dilluns';
+    $string['Tuesday'] = 'Dimarts';
+    $string['Wednesday'] = 'Dimecres';
+    $string['Thursday'] = 'Dijous';
+    $string['Friday'] = 'Divendres';
+    $string['Saturday'] = 'Dissabte';
+    $string['Sunday'] = 'Diumenge';
+    $string['of']= 'de';  
+    $string['logins']= 'Consultes de Logins'; 
+    $string['loginsko']= 'Consultes de Logins Fallits (Semàfor)';
+    $string['loginsuserko']= 'Consultes d\'Usuaris que no han pogut logar-se (Error)';
+    $string['logins']= 'Consultes de Logins';
+    $string['concurrents']= 'Consultes de concurrència';
+    $string['actius']= 'Usuaris Actius'; 
+    $string['activitats']= 'Consultes d\'Activitats'; 
+    $string['carregues']= 'Consultes de Càrregues'; 
+    $string['log_activitats']='Consulta Log d\'Activitat'; 
+    $string['logats']= 'Consultes els usuaris logats';  
+    $string['usuaris']= 'Usuaris';
+    $string['times']= 'Vegades';
+    $string['number_backends']= 'Mitjana de Backends';
+    $string['max']= 'Nombre Màxim';
+    $string['num_rows']= 'Nombre d\'Accions';
+    $string['quatrimestre']= 'Quatrimestre';
+    $string['campus']= 'Campus';
+    $string['not_found']= 'Usuari esborrat';
+    $string['durada']= 'Durada Total';
+    $string['max_date']= 'Data del Màxim';
+    $string['categoria']= 'Categoria';
+    $string['do_it_for_each_hour']='Fes-ho per hores';
+	$string['activity']='Consulta diaria d\'activitat';
+	$string['module']='Mòdul';
+	$string['reads']='Lectures';
+	$string['writes']='Escritures';
+	$string['select_clients']="Selecciona el client: ";
+	$string['rendiment']="Indicadors Rendiment";
+	$string['postgres']="Sessions postgres";
+	$string['idle']="Sessions idle";
+	$string['c1']="Càrrega 1'";
+	$string['c5']="Càrrega 5'";
+	$string['c15']="Càrrega 15'";
+	$string['smoodle'] = "Sessions Moodle";
+	$string['spostgres'] = "Sessions Postgres";
+	$string['consult_smoodle'] = "Consulta Sessions Moodle";
+	$string['instance']= "Instàncies";
+	$string['semafor']= "Semàfor";
+	$string['server_name']= "Back-End";
+	
+	
+?>
---
+++ lang-ca_utf8/service.php
@@ -0,0 +1,100 @@
+<?php
+
+//Inici strings d'Atenea as a service
+$string['config'] = 'Configuració dels entorns';
+$string['rendiment'] = 'Rendiment';
+$string['indicadors'] = 'Indicadors';
+$string['estatserveis'] = 'Estat dels serveis';
+$string['dadesclient'] = 'Dades del client';
+$string['dadesconexio'] = 'Dades de connexió al nou entorn';
+$string['dadesservei'] = 'Dades del servei';
+$string['nomclient'] = 'Nom del client';
+$string['usuconcurrents']='Usuaris concurrents contractats';
+$string['permisibilitat'] = 'Percentatge de permissibilitat';
+$string['semafor'] = 'Semàfor';
+$string['observacions'] = 'Observacions';
+$string['noentorn']= 'No hi ha cap entorn seleccionat';
+$string['noentornconfig'] = 'No hi ha cap entorn configurat';
+$string['usranalytics'] = 'Usuaris associats al entorn';
+
+
+$string['nomentorn']='Nom de l\'entorn';
+$string['tipusbd']='Tipus de BD';
+$string['hostbd']='Host BD de l\'entorn';
+$string['usuaribd']='Usuari de l\'entorn';
+$string['pswbd']='Password de l\'entorn';
+$string['portbd']='Port de l\'entorn';
+$string['nombd']='Nom de la BD';
+$string['persistenciabd']='Persistencia de l\'entorn';
+$string['prefix']='Prefix  de l\'entorn';
+
+
+//codis d'error
+$string['nodades'] = 'problemes al recuperar les dades';
+$string['nosavedades'] = 'No es poden guardar les dades. Torna-ho a intentar';
+$string['onlyadmin'] = 'Only the admin can use this page';
+$string['chooseenv'] = 'cal escollir un  entorn';
+$string['connectionproblem']='problemes de connexio';
+$string['noconfenv']="no s'ha pogut configurar l'entorn";
+
+//Fi strings d'Atenea as a service
+
+//Indicadors Del mòdul admupc
+$string['module'] = 'Mòdul';
+$string['data'] = 'Data';
+$string['quad'] = 'Quadrimestre';
+$string['rd'] = 'Lectures';
+$string['wr'] = 'Escriptures';
+$string['course'] = 'Curs';
+$string['vs'] = 'Visites';
+$string['vsu'] = 'Visites úniques';
+$string['gs'] = 'Convidats';
+$string['mx'] = 'Màxim conexions';
+$string['glog'] = 'Logins visitants';
+$string['racolog'] = 'Logins del Raco-FIB';
+$string['forum'] = 'Fòrum';
+$string['glossary'] = 'Glossari';
+$string['label'] = 'Etiqueta';
+$string['lesson'] = 'Lliçó';
+$string['quiz'] = 'Qüestionari';
+$string['wiki'] = 'Wiki';
+$string['assignment'] = 'Tasca';
+$string['calendar'] = 'Calendari';
+$string['chat'] = 'Xat';
+$string['choice'] = 'Consulta';
+$string['data'] = 'Base de dades';
+$string['grade'] = 'Qualificacions';
+$string['journal'] = 'Diari';
+$string['resource'] = 'Recurs';
+$string['scorm'] = 'SCORM';
+$string['survey'] = 'Enquesta';
+$string['workshop'] = 'Taller';
+$string['blog'] = 'Diari';
+$string['hotpot'] = 'Hotpot';
+$string['lams'] = 'Lams';
+$string['server_name']= "Back-End";
+
+//Global Stats
+$string['groupedstats']= "Indicadors Unificats";
+$string['groupstatstotal']= "Dates per tots els Clients";
+$string['selectstats']= "Estadística:";
+$string['norecordsfound']= "No s'han trobat registres";
+$string['errorrecords']= "Error en crear la Taula de resultats";
+$string['logins']= "Logins";
+$string['loginsko']= "Logins Fallits (Semàfor)";
+$string['loginsuserko']= "Logins Erronis";
+$string['actius']= 'Usuaris Actius'; 
+$string['logactivity']= 'Consulta log d\'Activitat'; 
+$string['logsessions']= 'Consulta Sessions Moodle de les instàncies';
+$string['env'] = 'Entorn';
+
+//Disc
+$string['discs'] = 'Consulta de Ocupació de Disc';
+$string['select_servers'] = 'Selecciona el Back-End :';
+$string['select_servers_be'] = 'Selecciona BE';
+$string['disc_history'] = 'Ocupació de Disc de';
+//Semàfor
+$string['semafors'] = 'Consulta del Semàfor';
+$string['semafor_info'] = 'Canvi del Semàfor';
+$string['semafor_history'] = 'Estat del Semàfor';
+?>
